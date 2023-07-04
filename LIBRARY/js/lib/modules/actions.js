ac5da7a6f5ab07101721ec3984feb2ad5056cce9
Kirill <93765186+Arsaide@users.noreply.github.com>
2023-07-05 01:50:59 +0300

    New Func
    Create new functions:
    1. closest() in actions.js
    2. siblings() in actions.js

--- LIBRARY/js/lib/modules/actions.js
+++ LIBRARY/js/lib/modules/actions.js
@@ -66,12 +66,49 @@ $.prototype.find = function(selector) {
     return this;
 };
 
+$.prototype.closest = function(selector) {
+    let counter = 0; // Сколько нашли элементов благодаря функции closest
 
+    for(let i = 0; i < this.length; i++) {
+        this[i] = this[i].closest(selector);
+        counter++;
+    }
 
+    const objLength = Object.keys(this).length;
+    for(; counter < objLength; counter++) {
+        delete this[counter];
+    }
 
+    return this;
+};
 
+$.prototype.siblings = function() {
+    let numberOfItems = 0, // Общее количество которое получилось
+        counter = 0; // Количество записанных элементов
 
+    const copyObj = Object.assign({}, this);
 
+    for(let i = 0; i < copyObj.length; i++) {
+        const arr = copyObj[i].parentNode.children; // каждый элемент найденный по селектору
 
+        for(let j = 0; j < arr.length; j++) {
+            if(copyObj[i] === arr[j]) {
+                continue;
+            }
 
+            this[counter] = arr[j];
+            counter++;
+        }
 
+        numberOfItems += arr.length - 1;    
+    }
+
+    this.length = numberOfItems;
+
+    const objLength = Object.keys(this).length;
+    for(; numberOfItems < objLength; numberOfItems++) {
+        delete this[numberOfItems];
+    }
+
+    return this;
+};
--- LIBRARY/js/main.js
+++ LIBRARY/js/main.js
@@ -8,6 +8,8 @@ $('div').click(function(){
     console.log($(this).index());
 });
 
-console.log($('div').eq(2).find('.more'));
+// console.log($('div').eq(2).find('.more'));
+// console.log($('.some').closest('.findme'));
+console.log($('.more').eq(0).siblings());
 
 // console.log($('button').html('Hello'));
