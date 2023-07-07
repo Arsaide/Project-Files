f09062d5c612bf99baf7d68a58102279b0100c3e
Kirill <93765186+Arsaide@users.noreply.github.com>
2023-07-07 23:25:44 +0300

    Dropdown menu
    Create new files:
    1. dropdown.scss in /components/ folder
    2. components folder in /lib/
    3. dropdown.js in /lib/components/
    
    Create new functions:
    1. dropdown in /dropdown/

--- LIBRARY/js/main.js
+++ LIBRARY/js/main.js
@@ -11,3 +11,17 @@ $('[data-count="second"]').on('click', () => {
 $('button').eq(2).on('click', () => {
     $('.w-500').fadeToggle(800);
 });
+
+$('.wrap').html(
+    `
+        <div class="dropdown">
+            <button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton">Dropdown button</button>
+            <div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
+                <a href="#" class="dropdown-item">Action №1</a>
+                <a href="#" class="dropdown-item">Action №2</a>
+                <a href="#" class="dropdown-item">Action №3</a>
+            </div>
+        </div>
+    `
+);
+$('.dropdown-toggle').dropdown();
